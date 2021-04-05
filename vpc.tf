1d10f69b78d71bdf75e7832647baa01cef4e1f3f
Eugene T <tooggi@gmail.com>
2021-04-05 14:29:21 +0300

    Fix IGW id

--- vpc.tf
+++ vpc.tf
@@ -78,6 +78,6 @@ resource "aws_route_table_association" "private_route_table_association" {
 resource "aws_route" "public_igw_route" {
   route_table_id = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main_igw
+  gateway_id = aws_internet_gateway.main_igw.id
 }
 
